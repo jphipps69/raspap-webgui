aa4f7d7d24a5100d7ed15a81e46765fcb413fb0e
Jeff Phipps <jphipps@penguinrandomhouse.com>
2018-04-03 12:07:54 -0400

    Remove AP signal data and make pass phrase a password field

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -6,129 +6,107 @@
  */
 function DisplayDashboard(){
 
-	$status = new StatusMessages();
+    $status = new StatusMessages();
 
-	/**
-	 *	Load Client Info
+    /**
+     *  Load Client Info
      */
-	exec( 'ip a s ' . RASPI_WIFI_CLIENT_INTERFACE , $return );
-	exec( 'iwconfig ' . RASPI_WIFI_CLIENT_INTERFACE, $return );
+    exec( 'ip a s ' . RASPI_WIFI_CLIENT_INTERFACE , $return );
+    exec( 'iwconfig ' . RASPI_WIFI_CLIENT_INTERFACE, $return );
 
-	$strWlan0 = implode( " ", $return );
-	$strWlan0 = preg_replace( '/\s\s+/', ' ', $strWlan0 );
+    $strWlan0 = implode( " ", $return );
+    $strWlan0 = preg_replace( '/\s\s+/', ' ', $strWlan0 );
 
-	// Parse results from ifconfig/iwconfig
-	preg_match( '/link\/ether ([0-9a-f:]+)/i',$strWlan0,$result ) || $result[1] = 'No MAC Address Found';
-	$strHWAddress = $result[1];
-	preg_match_all( '/inet ([0-9.]+)/i',$strWlan0,$result ) || $result[1] = 'No IP Address Found';
-	$strIPAddress = '';
-	foreach($result[1] as $ip) {
-		$strIPAddress .= $ip." ";
-	}
-	preg_match_all( '/[0-9.]+\/([0-3][0-9])/i',$strWlan0,$result ) || $result[1] = 'No Subnet Mask Found';
-	$strNetMask = '';
-	foreach($result[1] as $netmask) {
-		$strNetMask .= long2ip(-1 << (32 -(int)$netmask))." ";
-	}
-	preg_match( '/RX packets:(\d+)/',$strWlan0,$result ) || $result[1] = 'No Data';
-	$strRxPackets = $result[1];
-	preg_match( '/TX packets:(\d+)/',$strWlan0,$result ) || $result[1] = 'No Data';
-	$strTxPackets = $result[1];
-	preg_match( '/RX bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan0,$result ) || $result[1] = 'No Data';
-	$strRxBytes = $result[1];
-	preg_match( '/TX Bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan0,$result ) || $result[1] = 'No Data';
-	$strTxBytes = $result[1];
-	preg_match( '/ESSID:\"([a-zA-Z0-9\s].+)\"/i',$strWlan0,$result ) || $result[1] = 'Not connected';
-	$strSSID = str_replace( '"','',$result[1] );
-	preg_match( '/Access Point: ([0-9a-f:]+)/i',$strWlan0,$result ) || $result[1] = '';
-	$strBSSID = $result[1];
-	preg_match( '/Bit Rate=([0-9\.]+ Mb\/s)/i',$strWlan0,$result ) || $result[1] = '';
-	$strBitrate = $result[1];
-	preg_match( '/Tx-Power=([0-9]+ dBm)/i',$strWlan0,$result ) || $result[1] = '';
-	$strTxPower = $result[1];
-	preg_match( '/Link Quality=([0-9]+)/i',$strWlan0,$result ) || $result[1] = '';
-	$strLinkQuality = $result[1];
-	preg_match( '/Signal level=(-?[0-9]+ dBm)/i',$strWlan0,$result ) || $result[1] = '';
-	$strSignalLevel = $result[1];
-	preg_match('/Frequency:(\d+.\d+ GHz)/i',$strWlan0,$result) || $result[1] = '';
-	$strFrequency = $result[1];
+    // Parse results from ifconfig/iwconfig
+    preg_match( '/link\/ether ([0-9a-f:]+)/i',$strWlan0,$result ) || $result[1] = 'No MAC Address Found';
+    $strHWAddress = $result[1];
+    preg_match_all( '/inet ([0-9.]+)/i',$strWlan0,$result ) || $result[1] = 'No IP Address Found';
+    $strIPAddress = '';
+    foreach($result[1] as $ip) {
+        $strIPAddress .= $ip." ";
+    }
+    preg_match_all( '/[0-9.]+\/([0-3][0-9])/i',$strWlan0,$result ) || $result[1] = 'No Subnet Mask Found';
+    $strNetMask = '';
+    foreach($result[1] as $netmask) {
+        $strNetMask .= long2ip(-1 << (32 -(int)$netmask))." ";
+    }
+    preg_match( '/RX packets:(\d+)/',$strWlan0,$result ) || $result[1] = 'No Data';
+    $strRxPackets = $result[1];
+    preg_match( '/TX packets:(\d+)/',$strWlan0,$result ) || $result[1] = 'No Data';
+    $strTxPackets = $result[1];
+    preg_match( '/RX bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan0,$result ) || $result[1] = 'No Data';
+    $strRxBytes = $result[1];
+    preg_match( '/TX Bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan0,$result ) || $result[1] = 'No Data';
+    $strTxBytes = $result[1];
+    preg_match( '/ESSID:\"([a-zA-Z0-9\s].+)\"/i',$strWlan0,$result ) || $result[1] = 'Not connected';
+    $strSSID = str_replace( '"','',$result[1] );
+    preg_match( '/Access Point: ([0-9a-f:]+)/i',$strWlan0,$result ) || $result[1] = '';
+    $strBSSID = $result[1];
+    preg_match( '/Bit Rate=([0-9\.]+ Mb\/s)/i',$strWlan0,$result ) || $result[1] = '';
+    $strBitrate = $result[1];
+    preg_match( '/Tx-Power=([0-9]+ dBm)/i',$strWlan0,$result ) || $result[1] = '';
+    $strTxPower = $result[1];
+    preg_match( '/Link Quality=([0-9]+)/i',$strWlan0,$result ) || $result[1] = '';
+    $strLinkQuality = $result[1];
+    preg_match( '/Signal level=(-?[0-9]+ dBm)/i',$strWlan0,$result ) || $result[1] = '';
+    $strSignalLevel = $result[1];
+    preg_match('/Frequency:(\d+.\d+ GHz)/i',$strWlan0,$result) || $result[1] = '';
+    $strFrequency = $result[1];
 
-	if(strpos( $strWlan0, "UP" ) !== false) {
-		$status->addMessage('Client Interface is up', 'success');
-		$wlan0up = true;
-	} else {
-		$status->addMessage('Client Interface is down', 'warning');
-	}
-	
-	/** 
-	 *	Load Client Info
+    if(strpos( $strWlan0, "UP" ) !== false) {
+        $status->addMessage('Client Interface is up', 'success');
+        $wlan0up = true;
+    } else {
+        $status->addMessage('Client Interface is down', 'warning');
+    }
+    
+    /** 
+     *  Load Client Info
      */
      
-	exec( 'ip a s ' . RASPI_WIFI_AP_INTERFACE , $return2 );
-	exec( 'iwconfig ' . RASPI_WIFI_AP_INTERFACE, $return2 );
+    exec( 'ip a s ' . RASPI_WIFI_AP_INTERFACE , $return2 );
+    exec( 'iwconfig ' . RASPI_WIFI_AP_INTERFACE, $return2 );
 
-	$strWlan1 = implode( " ", $return2 );
-	$strWlan1 = preg_replace( '/\s\s+/', ' ', $strWlan1 );
+    $strWlan1 = implode( " ", $return2 );
+    $strWlan1 = preg_replace( '/\s\s+/', ' ', $strWlan1 );
 
-	// Parse results from ifconfig/iwconfig
-	preg_match( '/link\/ether ([0-9a-f:]+)/i',$strWlan1,$result ) || $result[1] = 'No MAC Address Found';
-	$strHWAddress2 = $result[1];
-	preg_match_all( '/inet ([0-9.]+)/i',$strWlan1,$result ) || $result[1] = 'No IP Address Found';
-	$strIPAddress2 = '';
-	foreach($result[1] as $ip) {
-		$strIPAddress2 .= $ip." ";
-	}
-	preg_match_all( '/[0-9.]+\/([0-3][0-9])/i',$strWlan1,$result ) || $result[1] = 'No Subnet Mask Found';
-	$strNetMask2 = '';
-	foreach($result[1] as $netmask) {
-		$strNetMask2 .= long2ip(-1 << (32 -(int)$netmask))." ";
-	}
-	preg_match( '/RX packets:(\d+)/',$strWlan1,$result ) || $result[1] = 'No Data';
-	$strRxPackets2 = $result[1];
-	preg_match( '/TX packets:(\d+)/',$strWlan1,$result ) || $result[1] = 'No Data';
-	$strTxPackets2 = $result[1];
-	preg_match( '/RX bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan1,$result ) || $result[1] = 'No Data';
-	$strRxBytes2 = $result[1];
-	preg_match( '/TX Bytes:(\d+ \(\d+.\d+ [K|M|G]iB\))/i',$strWlan1,$result ) || $result[1] = 'No Data';
-	$strTxBytes2 = $result[1];
-	preg_match( '/ESSID:\"([a-zA-Z0-9\s].+)\"/i',$strWlan1,$result ) || $result[1] = 'Not connected';
-	$strSSIDs = str_replace( '"','',$result[1] );
-	preg_match( '/Access Point: ([0-9a-f:]+)/i',$strWlan1,$result ) || $result[1] = '';
-	$strBSSIDs = $result[1];
-	preg_match( '/Bit Rate=([0-9\.]+ Mb\/s)/i',$strWlan1,$result ) || $result[1] = '';
-	$strBitrate2 = $result[1];
-	preg_match( '/Tx-Power=([0-9]+ dBm)/i',$strWlan1,$result ) || $result[1] = '';
-	$strTxPower2 = $result[1];
-	preg_match( '/Link Quality=([0-9]+)/i',$strWlan1,$result ) || $result[1] = '';
-	$strLinkQuality2 = $result[1];
-	preg_match( '/Signal level=(-?[0-9]+ dBm)/i',$strWlan1,$result ) || $result[1] = '';
-	$strSignalLevel2 = $result[1];
-	preg_match('/Frequency:(\d+.\d+ GHz)/i',$strWlan1,$result) || $result[1] = '';
-	$strFrequency2 = $result[1];
-
-	if(strpos( $strWlan1, "UP" ) !== false) {
-		$status->addMessage('AP Interface is up', 'success');
-		$wlan0up = true;
-	} else {
-		$status->addMessage('AP Interface is down', 'warning');
-	}
-	
-	if( isset($_POST['ifdown_wlan0']) ) {
-		exec( 'ifconfig ' . RASPI_WIFI_CLIENT_INTERFACE . ' | grep -i running | wc -l',$test );
-		if($test[0] == 1) {
-			exec( 'sudo ip link set ' . RASPI_WIFI_CLIENT_INTERFACE . ' down',$return );
-		} else {
-			echo 'Interface already down';
-		}
-	} elseif( isset($_POST['ifup_wlan0']) ) {
-		exec( 'ifconfig ' . RASPI_WIFI_CLIENT_INTERFACE . ' | grep -i running | wc -l',$test );
-		if($test[0] == 0) {
-			exec( 'sudo ip link set ' . RASPI_WIFI_CLIENT_INTERFACE . ' up',$return );
-			exec( 'sudo ip -s a f label ' . RASPI_WIFI_CLIENT_INTERFACE,$return);
-		} else {
-			echo 'Interface already up';
-		}
-	}
+    // Parse results from ifconfig/iwconfig
+    preg_match( '/link\/ether ([0-9a-f:]+)/i',$strWlan1,$result ) || $result[1] = 'No MAC Address Found';
+    $strHWAddress2 = $result[1];
+    preg_match_all( '/inet ([0-9.]+)/i',$strWlan1,$result ) || $result[1] = 'No IP Address Found';
+    $strIPAddress2 = '';
+    foreach($result[1] as $ip) {
+        $strIPAddress2 .= $ip." ";
+    }
+    preg_match_all( '/[0-9.]+\/([0-3][0-9])/i',$strWlan1,$result ) || $result[1] = 'No Subnet Mask Found';
+    $strNetMask2 = '';
+    foreach($result[1] as $netmask) {
+        $strNetMask2 .= long2ip(-1 << (32 -(int)$netmask))." ";
+    }
+    
+    if(strpos( $strWlan1, "UP" ) !== false) {
+        $status->addMessage('AP Interface is up', 'success');
+        $wlan0up = true;
+    } else {
+        $status->addMessage('AP Interface is down', 'warning');
+    }
+    
+    if( isset($_POST['ifdown_wlan0']) ) {
+        exec( 'ifconfig ' . RASPI_WIFI_CLIENT_INTERFACE . ' | grep -i running | wc -l',$test );
+        if($test[0] == 1) {
+            exec( 'sudo ip link set ' . RASPI_WIFI_CLIENT_INTERFACE . ' down',$return );
+        } else {
+            echo 'Interface already down';
+        }
+    } elseif( isset($_POST['ifup_wlan0']) ) {
+        exec( 'ifconfig ' . RASPI_WIFI_CLIENT_INTERFACE . ' | grep -i running | wc -l',$test );
+        if($test[0] == 0) {
+            exec( 'sudo ip link set ' . RASPI_WIFI_CLIENT_INTERFACE . ' up',$return );
+            exec( 'sudo ip -s a f label ' . RASPI_WIFI_CLIENT_INTERFACE,$return);
+        } else {
+            echo 'Interface already up';
+        }
+    }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
@@ -282,61 +260,17 @@ function DisplayDashboard(){
                                 </div><!-- /.panel-body -->
                             </div><!-- /.panel-default -->
                         </div><!-- /.col-md-6 -->
-                        
-                         <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-body wireless">
-                                    <h4>AP Wireless Information</h4>
-
-                                    <div class="info-item">
-                                        Connected To
-                                    </div><?php echo $strSSID2 ?><br>
-
-                                    <div class="info-item">
-                                        AP Mac Address
-                                    </div><?php echo $strBSSID2 ?><br>
-
-                                    <div class="info-item">
-                                        Bitrate
-                                    </div><?php echo $strBitrate2 ?><br>
-
-                                    <div class="info-item">
-                                        Signal Level
-                                    </div><?php echo $strSignalLevel2 ?><br>
-
-                                    <div class="info-item">
-                                        Transmit Power
-                                    </div><?php echo $strTxPower2 ?><br>
-
-                                    <div class="info-item">
-                                        Frequency
-                                    </div><?php echo $strFrequency2 ?><br>
-                                    <br>
-
-                                    <div class="info-item">
-                                        Link Quality
-                                    </div>
-
-                                    <div class="progress">
-                                        <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="<?php echo $strLinkQuality2 ?>" aria-valuemin="0" aria-valuemax="100" style="width: &lt;?php echo $strLinkQuality2 ?&gt;%;">
-                                            <?php echo $strLinkQuality2 ?>%
-                                        </div>
-                                    </div>
-                                </div><!-- /.panel-body -->
-                            </div><!-- /.panel-default -->
-                        </div><!-- /.col-md-6 -->
-
                     </div><!-- /.row -->
 
                     <div class="col-lg-12">
                         <div class="row">
                             <form action="?page=wlan0_info" method="post">
                                 <?php if ( !$wlan0up ) {
-		echo '<input type="submit" class="btn btn-success" value="Start ' . RASPI_WIFI_CLIENT_INTERFACE . '" name="ifup_wlan0" />';
-	} else {
-		echo '<input type="submit" class="btn btn-warning" value="Stop ' . RASPI_WIFI_CLIENT_INTERFACE . '" name="ifdown_wlan0" />';
-	}
-	?><input type="button" class="btn btn-outline btn-primary" value="Refresh" onclick="document.location.reload(true)">
+                                        echo '<input type="submit" class="btn btn-success" value="Start Cleint ' . RASPI_WIFI_CLIENT_INTERFACE . '" name="ifup_wlan0" />';
+                                    } else {
+                                        echo '<input type="submit" class="btn btn-warning" value="Stop Client ' . RASPI_WIFI_CLIENT_INTERFACE . '" name="ifdown_wlan0" />';
+                                    }
+                                    ?><input type="button" class="btn btn-outline btn-primary" value="Refresh" onclick="document.location.reload(true)">
                             </form>
                         </div>
                     </div>
@@ -349,8 +283,8 @@ function DisplayDashboard(){
         </div><!-- /.col-lg-12 -->
     </div><!-- /.row -->
     <?php
-}
+    }
 
-?>
+    ?>
 </body>
 </html>
