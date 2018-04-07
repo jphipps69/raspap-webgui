58b4633a245dea7f430076e30d6cf581dd846763
Jeff Phipps <jphipps@phipps.ws>
2018-04-07 10:05:07 -0400

    Add jquery locally

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $theme_url = 'dist/css/' . $theme;
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
 
 
 <script>
